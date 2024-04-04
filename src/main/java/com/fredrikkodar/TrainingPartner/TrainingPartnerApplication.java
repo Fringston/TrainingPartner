27b0da78809ecf6bb8230ee36030dcd18f9d6725
Fredrik <91frerin@gafe.molndal.se>
2024-04-04 13:53:29 +0200

    initial commit

---
+++ src/main/java/com/fredrikkodar/TrainingPartner/TrainingPartnerApplication.java
@@ -0,0 +1,13 @@
+package com.fredrikkodar.TrainingPartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingPartnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingPartnerApplication.class, args);
+	}
+
+}

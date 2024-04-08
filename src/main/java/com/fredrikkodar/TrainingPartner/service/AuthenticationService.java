744e969d6adf272f64d97777a76e862d24121d1f
Fredrik <91frerin@gafe.molndal.se>
2024-04-08 15:03:51 +0200

    Lagt till krav på säkrare lösenord

--- src/main/java/com/fredrikkodar/TrainingPartner/service/AuthenticationService.java
+++ src/main/java/com/fredrikkodar/TrainingPartner/service/AuthenticationService.java
@@ -16,6 +16,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 @Transactional
@@ -34,14 +36,22 @@ public class AuthenticationService {
 
 
    public User registerUser(String username, String password) {
-        String encodedPassword = passwordEncoder.encode(password);
-        Role userRole = roleRepository.findByAuthority("USER").get();
+       String passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+       Pattern pattern = Pattern.compile(passwordPattern);
+       Matcher matcher = pattern.matcher(password);
 
-        Set<Role> authorities = new HashSet<>();
-        authorities.add(userRole);
+       if (!matcher.matches()) {
+              throw new IllegalArgumentException("Password must contain at least one digit, one lowercase letter, one uppercase letter, one special character and be at least 8 characters long");
+       }
 
-        return userRepository.save(new User(0L, username, encodedPassword, authorities));
-    }
+       String encodedPassword = passwordEncoder.encode(password);
+       Role userRole = roleRepository.findByAuthority("USER").get();
+
+       Set<Role> authorities = new HashSet<>();
+       authorities.add(userRole);
+
+       return userRepository.save(new User(0L, username, encodedPassword, authorities));
+       }
 
     public LoginResponseDTO loginUser(String username, String password) {
         try {
@@ -53,17 +63,4 @@ public class AuthenticationService {
             return new LoginResponseDTO(null, "");
         }
     }
-
-    /*
-    // Ny LoginResponseDTO som endast skickar med jwt och skippar hela user-objektet
-    public LoginResponseDTO loginUser(String username, String password) {
-        try {
-            Authentication auth = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(username, password));
-            String token = tokenService.generateJwt(auth);
-            return new LoginResponseDTO(token);
-        } catch (AuthenticationException e) {
-            return new LoginResponseDTO("");
-        }
-    }*/
 }

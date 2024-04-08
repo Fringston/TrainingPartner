d2fd8a0467d56ecbaf5a68d496b9d08252ea59b1
Fredrik <91frerin@gafe.molndal.se>
2024-04-08 17:50:51 +0200

    getAllUsers

--- src/main/java/com/fredrikkodar/TrainingPartner/controller/AdminController.java
+++ src/main/java/com/fredrikkodar/TrainingPartner/controller/AdminController.java
@@ -1,18 +1,42 @@
 package com.fredrikkodar.TrainingPartner.controller;
 
+import com.fredrikkodar.TrainingPartner.dto.UserDTO;
+import com.fredrikkodar.TrainingPartner.entities.User;
+import com.fredrikkodar.TrainingPartner.exceptions.UserNotFoundException;
+import com.fredrikkodar.TrainingPartner.service.AdminService;
+import com.fredrikkodar.TrainingPartner.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
 @CrossOrigin("*")
 public class AdminController {
 
+    @Autowired
+    private AdminService adminService;
+
     @GetMapping
     public String helloAdminController() {
         return "Admin access level";
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        try {
+            List<UserDTO> users = adminService.getAllUsers();
+            return new ResponseEntity<>(users, HttpStatus.OK);
+        } catch (UserNotFoundException e){
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+
     }
 
 }
---
+++ src/main/java/com/fredrikkodar/TrainingPartner/service/AdminService.java
@@ -0,0 +1,51 @@
+package com.fredrikkodar.TrainingPartner.service;
+
+import com.fredrikkodar.TrainingPartner.dto.RoleDTO;
+import com.fredrikkodar.TrainingPartner.dto.UserDTO;
+import com.fredrikkodar.TrainingPartner.entities.User;
+import com.fredrikkodar.TrainingPartner.exceptions.UserNotFoundException;
+import com.fredrikkodar.TrainingPartner.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<UserDTO> getAllUsers() {
+    List<User> allUsers = userRepository.findAll();
+    List<UserDTO> allUsersDTO = new ArrayList<>();
+    for (User user : allUsers) {
+        allUsersDTO.add(convertToUserDTO(user));
+    } if (allUsersDTO.isEmpty()) {
+        throw new UserNotFoundException("No users found");
+        }
+        return allUsersDTO;
+}
+
+    private UserDTO convertToUserDTO(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUserId(user.getUserId());
+        userDTO.setUsername(user.getUsername());
+
+        Set<RoleDTO> roleDTOs = user.getAuthorities().stream()
+                .map(role -> {
+                    RoleDTO roleDTO = new RoleDTO();
+                    roleDTO.setAuthority(role.getAuthority());
+                    return roleDTO;
+                })
+                .collect(Collectors.toSet());
+
+        userDTO.setRoles(roleDTOs);
+        return userDTO;
+    }
+
+}
